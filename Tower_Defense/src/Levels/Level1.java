e823d874c0989d93fdff01ea5b6de21bf124c078
Ludwig Kittel <ludwig.kittel98@me.com>
2016-02-17 09:31:39 +0100

    Hab paar Buttons im Level1 gemacht

--- Tower_Defense/src/Levels/Level1.java
+++ Tower_Defense/src/Levels/Level1.java
@@ -1,21 +1,42 @@
 package Levels;
-import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JToggleButton;
 
 public class Level1 {
-	public static void main(String[] args){      
+public static void main(String[] args){      
         
 		JFrame l1 = new JFrame("Level 1");      
         l1.setSize(700,750);
         l1.setLocationRelativeTo(null);
+       
+        JPanel jp = new JPanel();
+        jp.setSize(new Dimension(80,10));
+        
+        
+        JToggleButton nt = new JToggleButton("<html><center>Normal<br>Tower</center></html>",false);
+        jp.add(nt);
+        nt.setPreferredSize(new Dimension(90,75));
+        
+        JToggleButton st = new JToggleButton("<html><center>Slow<br>Tower</center></html>",false);
+        jp.add(st);
+        st.setPreferredSize(new Dimension(90,75));
+        
+        JToggleButton hrt = new JToggleButton("<html><center>High-Rage<br>Tower</center></html>",false);
+        jp.add(hrt);
+        hrt.setPreferredSize(new Dimension(110,75));
+        
+        JToggleButton ft = new JToggleButton("<html><center>Flame<br>Tower</center></html>",false);
+        jp.add(ft);
+        ft.setPreferredSize(new Dimension(90,75));
+        
+ 
+        l1.add(jp);
         l1.setVisible(true);
         l1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        JPanel p1 = new JPanel();
-        
-        JButton b1 = new JButton("Test");
-        p1.add(b1);
-        l1.add(p1);
-    }
+	}
 }
+
